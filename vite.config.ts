db247ac11d868f10d3dc58037f7700a97f31b3bd
Carlos Fernando Arroyo Garfias <110379162+CarlosArroyoG@users.noreply.github.com>
2026-01-16 09:21:27 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
-});
+  plugins: [react()],
+  base: '/',          // 👈 importante para producción
+  build: {
+    outDir: 'dist',   // 👈 carpeta que usará Coolify
+    assetsDir: 'assets',
+    sourcemap: false // 👈 opcional, recomendado en prod
+  }
+})
